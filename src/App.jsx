2f656254850393ff8b81d6cf77a0b16423295198
harisali <haris.ali@unikrew.com>
2024-08-14 18:58:56 +0500

    Add Toast for notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './styles/styles.css';
 import Main from "./components/main.jsx";
 import {OpulentSipsProvider} from "./context/context.jsx";
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
@@ -8,6 +10,7 @@ const App = () => {
     return <>
         <OpulentSipsProvider>
             <Main/>
+            <ToastContainer/>
         </OpulentSipsProvider>
     </>;
 };
